19e936d78c3373cf82a7e9ef731a5a326b787191
Sean Febo <seanfebo@techie.com>
2018-04-12 21:13:54 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -4,7 +4,7 @@ class CashRegister
     @total = 0 
     @discount = discount
   end
-  def add_item(title, price, quantity = 0)
+  def add_item(title, price, quantity = 1)
     @total += price*quantity
   end
     
